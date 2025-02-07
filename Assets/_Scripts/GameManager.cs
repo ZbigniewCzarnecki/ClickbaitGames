4be73a0f17c6958e7287831b06ad8332b44bcad5
Zbigniew Czarnecki <z.czarnecki21@gmail.com>
2025-02-07 19:03:34 +0100

    Improved spawning system

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -3,30 +3,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Player _playerPrefab;
+    
     public static GameManager Instance;
-
+    
+    private GroundSpawner _groundSpawner;
+    
+    public GroundSpawner GroundSpawner { get { return _groundSpawner; } }
+    
     public enum GameState
     {
-        WaitingToStart,
+        WaitingForStart,
         Gameplay,
         Win
     }
     
-    private GameState _gameState = GameState.WaitingToStart;
+    private GameState _gameState = GameState.WaitingForStart;
     
     private void Awake()
+    {
+        InitializeInstance();
+        _groundSpawner = GetComponentInChildren<GroundSpawner>();
+    }
+
+    private void Start()
+    {
+        SpawnPlayer();
+    }
+
+    private void InitializeInstance()
     {
         if (Instance != null)
         {
             Destroy(gameObject);
             return;
         }
-        
+
         Instance = this;
     }
 
     public void SetNewGameState(GameState gameState)
     {
         _gameState = gameState;
+        OnGameStateChanged(_gameState);
+    }
+
+    private void OnGameStateChanged(GameState gameState)
+    {
+        switch (gameState)
+        {
+            case GameState.WaitingForStart:
+                break;
+            case GameState.Gameplay:
+                break;
+            case GameState.Win:
+                break;
+        }
+    }
+    
+    public void SpawnPlayer()
+    {
+        Instantiate(_playerPrefab.transform, _playerPrefab.transform.position, Quaternion.identity);
     }
 }
--- Assets/_Scripts/GroundSpawner.cs
+++ Assets/_Scripts/GroundSpawner.cs
@@ -5,27 +5,49 @@ public class GroundSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _groundPrefab;
     private NavMeshSurface _navMeshSurface;
-
+    
+    private float _groundSizeZ;
+    private float _newZPos;
+    
     private void Awake()
     {
         _navMeshSurface = GetComponent<NavMeshSurface>();
+        _groundSizeZ = _groundPrefab.GetComponent<BoxCollider>().size.z;
+        
+        SpawnGround(2);
+    }
+    
+    public void SpawnGround()
+    {
+        SetNewSpawnLocation();
+        SpawnNewTile();
+        BuildNewNavMesh();
     }
 
-    private void Start()
+    private void SpawnGround(int howMany)
     {
-        float newZPos = 0;
-        
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i < howMany; i++)
         {
-            newZPos += _groundPrefab.localScale.z;
-            
-            Instantiate(_groundPrefab,
-                new Vector3(_groundPrefab.position.x, _groundPrefab.position.y, _groundPrefab.position.z + newZPos),
-                Quaternion.identity);
+            SetNewSpawnLocation();
+            SpawnNewTile();
+            BuildNewNavMesh();
         }
-        
+    }
+
+    private void SetNewSpawnLocation()
+    {
+        _newZPos += _groundSizeZ;
+    }
+
+    private void SpawnNewTile()
+    {
+        Instantiate(_groundPrefab,
+            new Vector3(_groundPrefab.position.x, _groundPrefab.position.y, _groundPrefab.position.z + _newZPos),
+            Quaternion.identity);
+    }
+    
+    private void BuildNewNavMesh()
+    {
         _navMeshSurface.BuildNavMesh();
-        
-        PlayerSpawner.Instance.SpawnPlayer();
     }
 }
